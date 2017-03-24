d43197b0e8dbeb30f7e88ddb9426413da429c656
daffron <daffron92@gmail.com>
2017-03-24 15:19:24 +1300

    working on landing pages

--- js/script.js
+++ js/script.js
@@ -1,64 +1,225 @@
  window.onload=function(){
+var tally = {
 
-var xero = 0;
-var reckon = 0;
-var quickbooks = 0;
+ myob: 0,
+ xero:  0,
+ reckon: 0,
+ quickbooks: 0
+};
 
-
-document.getElementById('#go').addEventListener('click', function(){
+//Start
+document.getElementById('go').addEventListener('click', function(){
 questionOne();
 });
-document.getElementById('#macos').addEventListener('click',function(){
+
+//What Operating system
+document.getElementById('macos').addEventListener('click',function(){
   addXero();
   questionThree();
 });
-document.getElementById('#windows').addEventListener('click',function(){
+document.getElementById('windows').addEventListener('click',function(){
   questionThree();
   addXero();
 });
-document.getElementById('#linux').addEventListener('click', function(){
+document.getElementById('linux').addEventListener('click', function(){
   questionThree();
 });
-document.getElementById('#large').addEventListener('click', function(){
+
+//How big
+document.getElementById('large').addEventListener('click', function(){
+  addXero();
   questionFour();
+});
+document.getElementById('medium').addEventListener('click', function(){
   addXero();
+  questionFour();
+});
+document.getElementById('small').addEventListener('click', function(){
+  addXero();
+  questionFour();
+});
+
+//Budget
+document.getElementById('low').addEventListener('click', function(){
+  addXero();
+  addQuickbooks();
+  questionFive();
+});
+document.getElementById('average').addEventListener('click', function(){
+  addXero();
+  questionFive();
+});
+document.getElementById('high').addEventListener('click', function(){
+  addMyob();
+  questionFive();
+});
+
+//payroll
+document.getElementById('payyes').addEventListener('click', function(){
+  addXero();
+  addMyob();
+  questionSix();
+});
+document.getElementById('payno').addEventListener('click', function(){
+  addXero();
+  addQuickbooks();
+  questionSix();
+});
+
+//invoicing
+document.getElementById('invyes').addEventListener('click', function(){
+  addXero();
+  addMyob();
+  questionSeven();
+});
+document.getElementById('invno').addEventListener('click', function(){
+  addXero();
+  addQuickbooks();
+  questionSeven();
+});
+
+//banking
+document.getElementById('bankyes').addEventListener('click', function(){
+  addXero();
+  addMyob();
+  questionEight();
+});
+document.getElementById('bankno').addEventListener('click', function(){
+  addXero();
+  addQuickbooks();
+  questionEight();
+});
+
+//how many invoices per month
+document.getElementById('lessthan30').addEventListener('click', function(){
+  addXero();
+  addMyob();
+  sort();
+});
+document.getElementById('lessthan100').addEventListener('click', function(){
+  addXero();
+  addQuickbooks();
+  sort();
+});
+document.getElementById('morethan100').addEventListener('click', function(){
+  addXero();
+  addQuickbooks();
+  sort();
 });
 
 
 
+//Add Functions
 function addXero(){
-  xero ++;
-
+  tally.xero ++;
 };
 function addReckon(){
-  reckon++;
+  tally.reckon++;
 };
 function addQuickbooks(){
-  quickbooks++;
+  tally.quickbooks++;
 };
+function addMyob(){
+  tally.myob++;
+};
+
+//Questions
 function questionOne() {
-  var one = document.getElementById('#start')
-  console.log(one);
+  var one = document.getElementById('start')
   one.classList.add('hide');
-  var two = document.getElementById('#os')
+  var two = document.getElementById('os')
   two.classList.remove('hide')
+  console.log(tally);
 };
 
 function questionThree() {
-  var i = document.getElementById('#os')
+  var i = document.getElementById('os')
   i.classList.add('hide');
-  var x = document.getElementById('#howbig')
+  var x = document.getElementById('howbig')
   x.classList.remove('hide')
-  console.log(xero,quickbooks,reckon)
+  console.log(tally)
 };
 
 function questionFour(){
-  var i = document.getElementById('#howbig')
+  var i = document.getElementById('howbig')
   i.classList.add('hide');
-  var x = document.getElementById('#budget')
+  var x = document.getElementById('budget')
   x.classList.remove('hide')
-  console.log(xero,quickbooks,reckon);
+  console.log(tally);
 };
 
+function questionFive(){
+  var i = document.getElementById('budget')
+  i.classList.add('hide');
+  var x = document.getElementById('payroll')
+  x.classList.remove('hide')
+  console.log(tally);
+};
 
+function questionSix(){
+  var i = document.getElementById('payroll')
+  i.classList.add('hide');
+  var x = document.getElementById('invoice')
+  x.classList.remove('hide')
+  console.log(tally);
+};
+
+function questionSeven(){
+  var i = document.getElementById('invoice')
+  i.classList.add('hide');
+  var x = document.getElementById('bank')
+  x.classList.remove('hide')
+  console.log(tally);
+};
+
+function questionEight(){
+  var i = document.getElementById('bank')
+  i.classList.add('hide');
+  var x = document.getElementById('howmanyinvoice')
+  x.classList.remove('hide')
+  console.log(tally);
+};
+
+//Routing Code
+function sort(){
+if (tally.xero > tally.reckon && tally.xero > tally.myob && tally.xero >tally.quickbooks){
+  openXero();
+}
+else if (tally.quickbooks > tally.xero && tally.quickbooks > tally.reckon && tally.quickbooks > tally.myob){
+  openQuickbooks();
+}
+else if (tally.reckon > tally.xero && tally.reckon > tally.quickbooks && tally.reckon > tally.myob){
+  openReckon();
+}
+else {
+  openMyob();
+}
+};
+
+function openXero(){
+  var url = "xero.html";
+  window.open(url, '_top');
+  }
+
+/*
+function openQuickbooks(){
+  var i = document.getElementById('howmanyinvoice')
+  i.classList.add('hide');
+  var x = document.getElementById('xerolanding')
+  x.classList.remove('hide')
+}
+
+function openReckon(){
+  var i = document.getElementById('howmanyinvoice')
+  i.classList.add('hide');
+  var x = document.getElementById('')
+  x.classList.remove('hide')
+}
+
+function openMyob(){
+  var i = document.getElementById('howmanyinvoice')
+  i.classList.add('hide');
+  var x = document.getElementById('')
+  x.classList.remove('hide')
+}*/
 }; //End of onload function
